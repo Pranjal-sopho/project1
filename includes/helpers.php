c47abaac042279b8fa88a34181157f03946ee6d9
Pranjal Sharma <pranjal.sharmaxxx@gmail.com>
2017-03-02 19:29:29 +0000

    some optmization done for better performance

--- includes/helpers.php
+++ includes/helpers.php
@@ -15,7 +15,7 @@
         $string = file_get_contents($college_link);
         
         // extracting college name
-        preg_match('/(?<=class="head-3 right-head">)(.*)<s/',$string,$address);
+        preg_match('/(?<=<title>)(.*),/',$string,$name);
         
         // get year of establishment
         preg_match('/(?<=Established ).*? /',$string,$yoe);
@@ -28,15 +28,69 @@
         
         // finding courses offered
         $courses = [];
+        $course = [];
         $i=0;
+        
+        // storing all the courses available in $courses
         preg_match_all('/(?<= class="li-dropdown-a").*</',$string,$courses);
+        
+        // trimming and storing them in $course
         while(true)
         {
-          preg_match('/(?<=">)(.*),/',$courses[$i][0],$course);
-          $courses[i] = $course[1];
+          if(preg_match('/(?<=">)(.*),/',$courses[$i][0],$temp) == false)
+            break;
+          $course[i] = $temp[1];
           $i = $i+1;
-           
         }
         
+        // extracting college's infrastructural info
+        $i=0; 
+        preg_match_all('/(?<=<a class=").*/',$string,$ans);
+        while(true)
+        {
+            if(preg_match('/(?<=">).*? /',$ans[0][$i],$infra[$i])== false)
+                break;
+            $i = $i+1;
+        }
+        
+        // attempting to connect to mysql server
+        $link = mysqli_connect("127.0.0.1", "pranjal123321", "zrrJ8zNEdpuTwuty", "project1");
+        if($link === false)
+            die("ERROR: Could not connect. " . mysqli_connect_error());
+            
+        // attempting insert query
+        $query = "INSERT INTO college_info (Name, Address,Year of establishment,College's Website) VALUES ($name, $address, $yoe,$website)";
+        if(mysqli_query($link, $sql))
+        {
+            echo "ERROR: Could not execute $sql. " . mysqli_error($link);
+        }
+        
+        // get college id
+        $query = "SELECT Serial_number FROM college_info WHERE Name = $name";
+        $id = mysqli($link,$query);
+        
+        // inserting the courses and infra fields in respective tables
+        $i=0;
+        while(true)
+        {
+            if($course[$i]== NULL)
+                break;
+            $query = "INSERT INTO courses (serial number,courses_offered) VALUES ($id, $course[$i])";
+            mysqli_query($link, $query);
+            $i = $i+1;
+        }
+    
+        $i=0;
+        while(true)
+        {
+            if($infra[$i]== NULL)
+                break;
+            $query = "INSERT INTO infrastructure (serial number,facilities) VALUES ($id, $infra[$i])";
+            mysqli_query($link, $query);
+            $i = $i+1;   
+        }
+        
+        // closing connection
+        mysqli_close($link);
     }
 ?>
--- public/index.php
+++ public/index.php
@@ -28,39 +28,17 @@
     
         else
         {
-            // attempting to connect to mysql server
-            $link = mysqli_connect("127.0.0.1", "pranjal123321", "zrrJ8zNEdpuTwuty", "project1");
-            if($link === false)
-                die("ERROR: Could not connect. " . mysqli_connect_error());
-                
+            preg_match('/(?<=class="tuple-clg-heading")(.*)<\/a>/',$string,$college_link);
+            
             while(true)
             {
                 // extract college's link from the scraped data
-                preg_match('/(?<=class="tuple-clg-heading")(.*)<\/a>/',$string,$college_link);
+               
                 preg_match('/(?<=href=")(.*)" target/',$college_link[1],$college_link);
                 
+                // passing the college link for scraping data furthur and storing in database
                 get_college_info($college_link);
-               // preg_match('/(?<=target="_blank">)(.*)/',$name[1],$name);
                 
-                preg_match('/.*(?=<\/p><\/h2>)/',$string,$address);
-                preg_match('/(?<=\\s\\s\\s).*/',$address[0],$address);
-                preg_match('/(?<=<p>\| ).*/',$address[0],$aaddress);
-                
-                $yoe = preg_match();
-                $courses = preg_match();
-                $infra = preg_match();
-                $website = preg_match();
-                
-                // attempting insert query
-                $query = "INSERT INTO college_info (name, address,yoe,courses,infra,website) VALUES ($name, $address, $yoe,$courses,$infra,$website)";
-                
-                if(mysqli_query($link, $sql))
-                {
-                    echo "ERROR: Could not execute $sql. " . mysqli_error($link);
-                }
-                
-                // close connection
-                mysqli_close($link);
             }
             
         }
