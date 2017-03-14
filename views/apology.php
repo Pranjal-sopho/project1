221f635d80834f456277f16807d6eed3b65419d5
Pranjal Sharma <pranjal.sharmaxxx@gmail.com>
2017-03-14 18:40:51 +0000

    aesthetics of the site improved and css added

---
+++ views/apology.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Sorry!</title>
+</head>
+<body>
+    <p>
+       <?= print($message); ?>
+    </p>
+</form>
+</body>
+</html>
--- views/form.php
+++ views/form.php
@@ -1,22 +1,54 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
 <head>
 <meta charset="UTF-8">
-<title><?= $title ?></title>
+<title>Welcome</title>
+
+<link rel="stylesheet" href="style.css">
 </head>
 <body>
-<form action="index.php" method="post">
-   
-    <p>
-       Select the city of your choice
-    </p>
-    <p>
-       
-       <div class="form-group">
-            <input autocomplete="off" autofocus class="form-control" name="city" placeholder="Enter city" type="text"/>
+<!-- Start here -->
+<div class="content">
+    <h1>
+    	The College Finder
+    </h1>
+</div>
+    <section>
+    <div class="form-group">
+            <input autocomplete="off" autofocus class="form-control" name="city" placeholder="Enter City" type="text"/>
+    </div>
+    <div>
+    <button class="but" type="submit">Submit</button>
+    </div>        
+    </section>
+	
+    <footer>
+    <div class="footer-content">
+            <ul class="footer-social">
+                <li>
+                    <a href="https://www.facebook.com/pranjal.sharma.39904">
+                        <img class="social-default" src="images/social-facebook.png">
+                        <img class="social-hover" src="images/social-facebook-hover.png">
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/Pranjal-sopho">
+                        <img class="social-default" src="images/Github.png">
+                        <img class="social-hover" src="images/Github.png">
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/pranjal-sharma-34b997113/">
+                        <img class="social-default" src="images/download.png">
+                        <img class="social-hover" src="images/download.png">
+                    </a>
+                </li>
+            </ul>
+            <div class="footer-info">
+                <p class="footer-credit">Website design by <a href="#">Pranjal Sharma</a></p>
+                <p class="footer-legal">&copy; 2017. All rights reserved.</p>
+            </div>
         </div>
-    </p>
-    <input type="submit" value="Submit">
-</form>
+    </footer>
 </body>
 </html>
